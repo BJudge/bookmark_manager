16598ad80c2f12fe2811f9a7e8305e6cc167c20d
Zeeshan Ishtiaq <mzishtiaq@yahoo.co.uk>
2016-07-29 13:05:31 +0100

    adds validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,15 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
-  property :email, String
+  property :email, String, required: true
   property :password_digest, String, length: 60
 
   attr_accessor :confirm_password
   attr_reader :password
 
   validates_confirmation_of :password, confirm: :confirm_password
+  validates_presence_of :email
+  validates_format_of :email, as: :email_address
 
   def password=(password)
     @password = password
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -10,4 +10,12 @@ feature 'User sign up' do
     expect(current_path).to eq '/users'
     expect(page).to have_content 'Password and confirmation password do not match'
   end
+
+  scenario "user cannot sign-up with a blank email address" do
+    expect { sign_up(email: nil) }.not_to change(User, :count)
+  end
+
+  scenario "user can't sign-up with an invalid email address" do
+    expect { sign_up(email: 'invalid@gmail')}.not_to change(User, :count)
+  end
 end
